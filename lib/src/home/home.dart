3b318cddb06c188af039363c0badec8bf481a9b0
linkongwu <578310571@qq.com>
2020-04-29 18:49:40 +0800

    3.3.4

--- lib/src/home/home.dart
+++ lib/src/home/home.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:fluttercnte/src/test/random_text.dart';
 import 'package:fluttercnte/src/test/tapbox_a.dart';
 import 'package:fluttercnte/src/test/tapbox_b.dart';
-import '../route/new_route.dart';
-import '../cuptino/cuption_route.dart';
-import '../test/tapbox_c.dart';
+import 'package:fluttercnte/src/route/new_route.dart';
+import 'package:fluttercnte/src/cuptino/cuption_route.dart';
+import 'package:fluttercnte/src/test/tapbox_c.dart';
+import 'package:fluttercnte/src/test/text_test.dart';
 
 class Home extends StatefulWidget {
   Home({Key key, this.title}) : super(key: key);
@@ -29,18 +30,103 @@ class _HomeState extends State<Home> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: <Widget>[
-            new Text("敲击加号增加敲击次数"),
-            new Text(
-              "$_count",
-              style: Theme.of(context).textTheme.display4,
+            Column(
+              children: <Widget>[
+                new Text("敲击加号增加敲击次数"),
+                new Text(
+                  "$_count",
+                  style: Theme.of(context).textTheme.display4,
+                ),
+                FlatButton(
+                  textColor: Colors.brown,
+                  child: Text(
+                    "打开新页面",
+                    style: TextStyle(fontSize: 16.6),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            maintainState: false,
+                            builder: (context) {
+                              return RouterTestRoute();
+                            }));
+                  },
+                ),
+                RandomWordsWight(),
+                CuptinoTestRoute(),
+                FlatButton(
+                  textColor: Colors.brown,
+                  child: Text(
+                    "打开CuptinoTestRoute页面",
+                    style: TextStyle(fontSize: 16.6),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            maintainState: false,
+                            builder: (context) {
+                              return CuptinoTestRoute();
+                            }));
+                  },
+                ),
+                FlatButton(
+                  textColor: Colors.brown,
+                  child: Text(
+                    "打开TapboxA页面",
+                    style: TextStyle(fontSize: 16.6),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            maintainState: false,
+                            builder: (context) {
+                              return TapboxA();
+                            }));
+                  },
+                ),
+                FlatButton(
+                  textColor: Colors.brown,
+                  child: Text(
+                    "打开TapboxB页面",
+                    style: TextStyle(fontSize: 16.6),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            maintainState: false,
+                            builder: (context) {
+                              return ParentWidget();
+                            }));
+                  },
+                ),
+                FlatButton(
+                  textColor: Colors.brown,
+                  child: Text(
+                    "打开TapboxC页面",
+                    style: TextStyle(fontSize: 16.6),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            maintainState: false,
+                            builder: (context) {
+                              return ParentWidgetC();
+                            }));
+                  },
+                ),
+              ],
             ),
             FlatButton(
               textColor: Colors.brown,
               child: Text(
-                "打开新页面",
+                "打开文本测试页面",
                 style: TextStyle(fontSize: 16.6),
               ),
               onPressed: () {
@@ -49,73 +135,7 @@ class _HomeState extends State<Home> {
                     MaterialPageRoute(
                         maintainState: false,
                         builder: (context) {
-                          return RouterTestRoute();
-                        }));
-              },
-            ),
-            RandomWordsWight(),
-            CuptinoTestRoute(),
-            FlatButton(
-              textColor: Colors.brown,
-              child: Text(
-                "打开CuptinoTestRoute页面",
-                style: TextStyle(fontSize: 16.6),
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        maintainState: false,
-                        builder: (context) {
-                          return CuptinoTestRoute();
-                        }));
-              },
-            ),
-            FlatButton(
-              textColor: Colors.brown,
-              child: Text(
-                "打开TapboxA页面",
-                style: TextStyle(fontSize: 16.6),
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        maintainState: false,
-                        builder: (context) {
-                          return TapboxA();
-                        }));
-              },
-            ),
-            FlatButton(
-              textColor: Colors.brown,
-              child: Text(
-                "打开TapboxB页面",
-                style: TextStyle(fontSize: 16.6),
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        maintainState: false,
-                        builder: (context) {
-                          return ParentWidget();
-                        }));
-              },
-            ),
-            FlatButton(
-              textColor: Colors.brown,
-              child: Text(
-                "打开TapboxC页面",
-                style: TextStyle(fontSize: 16.6),
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        maintainState: false,
-                        builder: (context) {
-                          return ParentWidgetC();
+                          return TextTestPage();
                         }));
               },
             ),
---
+++ lib/src/test/text_test.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class TextTestPage extends StatefulWidget {
+  @override
+  _TextTestPageState createState() => _TextTestPageState();
+}
+
+class _TextTestPageState extends State<TextTestPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("文本测试"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Text(
+                "HELLO WORLD!",
+                textAlign: TextAlign.left,
+              ),
+              Text(
+                "HELLO WORLD!" * 6,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                "HELLO WORLD!" * 6,
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "HELLO WORLD!",
+                textScaleFactor: 1.5,
+              ),
+              Text(
+                "HELLO WORLD!",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 20.0,
+                  fontStyle: FontStyle.italic,
+                  fontFamily: "courier",
+                  decoration: TextDecoration.underline,
+                  decorationStyle: TextDecorationStyle.dashed,
+                  background: new Paint()..color = Colors.yellow,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Text.rich(
+                TextSpan(children: [
+                  TextSpan(text: "home"),
+                  TextSpan(
+                    text: "http://git/flutter_cn_te",
+                    style: TextStyle(color: Colors.blue),
+                    // recognizer: ConnectionState.active//手势识别
+                  ),
+                ]),
+              ),
+              DefaultTextStyle(
+                style: TextStyle(color: Colors.red, fontSize: 20.0),
+                textAlign: TextAlign.start,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("defalt text"),
+                    Text("I am an A"),
+                    Text(
+                      "I am a K",
+                      style: TextStyle(inherit: false, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
