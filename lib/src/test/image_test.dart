d732d1759b1b3819c06ce7142e2a645ca24ccd4c
linkongwu <578310571@qq.com>
2020-04-29 21:46:27 +0800

    3.5.1 image

--- lib/src/test/image_test.dart
+++ lib/src/test/image_test.dart
@@ -12,9 +12,68 @@ class _ImageTestState extends State<ImageTest> {
       appBar: AppBar(
         title: Text("Image test page"),
       ),
-      body: Column(
+      body: ListView(
         children: <Widget>[
-          Image(image: AssetImage("lib\src\images\1.jpg"),)
+          Image.asset(
+            "assets/images/1.jpg",
+            height: 50.0,
+            width: 100.0,
+          ),
+          Image.asset(
+            "assets/images/1.jpg",
+            fit: BoxFit.values[0],
+            height: 50.0,
+            width: 100.0,
+          ),
+          Image.asset(
+            "assets/images/1.jpg",
+            fit: BoxFit.values[1],
+            height: 50.0,
+            width: 100.0,
+          ),
+          Image.asset(
+            "assets/images/1.jpg",
+            fit: BoxFit.values[2],
+            height: 50.0,
+            width: 100.0,
+          ),
+          Image.asset(
+            "assets/images/1.jpg",
+            fit: BoxFit.values[3],
+            height: 50.0,
+            width: 100.0,
+          ),
+          Image.asset(
+            "assets/images/1.jpg",
+            fit: BoxFit.values[4],
+            height: 50.0,
+            width: 100.0,
+          ),
+          Image.asset(
+            "assets/images/1.jpg",
+            fit: BoxFit.values[5],
+            height: 50.0,
+            width: 100.0,
+          ),
+          Image.asset(
+            "assets/images/1.jpg",
+            fit: BoxFit.values[6],
+            height: 50.0,
+            width: 100.0,
+          ),
+          Image.asset(
+            "assets/images/1.jpg",
+            height: 50.0,
+            width: 100.0,
+            color: Colors.blue,
+            colorBlendMode: BlendMode.plus,
+          ),
+          Image.network(
+            "https://dss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2534506313,1688529724&fm=26&gp=0.jpg",
+            width: 100,
+            height: 600,
+            repeat: ImageRepeat.repeatY,
+          )
         ],
       ),
     );
